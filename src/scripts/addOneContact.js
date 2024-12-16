7db3349a53ac49b787109467cbb7121a756cfbb5
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-12-16 14:58:43 +0200

    add finish

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,10 +1,10 @@
 import {readContacts} from "../utils/readContacts.js";
-import {createFakeContacts} from "../utils/createFakeContact.js";
+import {createFakeContact} from "../utils/createFakeContact.js";
 import {writeContacts} from "../utils/writeContacts.js";
 
 export const addOneContact = async () => {
     const contactList = await readContacts();
-    const contact = createFakeContacts();
+    const contact = createFakeContact();
     await writeContacts([...contactList, contact]);
 };
 
